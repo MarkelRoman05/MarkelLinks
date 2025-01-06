3ab608d1208741149f41de442f01b0ae9ffe581e
Markel <markel.romango@elorrieta-errekamari.com>
2025-01-06 22:21:28 +0100

    add supercopa de españa text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { AgendaEventoTextComponent } from './agenda-evento-text/agenda-evento-text.component';
 
 declare global {
  interface Window {
@@ -25,6 +26,7 @@ declare global {
   FormsModule,
   MatIconModule,
   ReactiveFormsModule,
+  AgendaEventoTextComponent,
  ],
  templateUrl: './app.component.html',
  styleUrl: './app.component.css',
