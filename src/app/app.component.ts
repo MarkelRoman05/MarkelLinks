cf3d0b58969cb21351eba0235554cd7d3ca0fdf5
MarkelRoman05 <markromagonz@gmail.com>
2025-05-14 19:59:10 +0200

    Agregar condición para mostrar mensaje informativo en función de la URL y definir la ubicación de la ventana en el componente principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -42,6 +42,8 @@ export class AppComponent {
   isImagePopupOpen: boolean = false;
   popupImageUrl: string = '';
 
+  location = window.location;
+
   constructor(private snackBar: MatSnackBar) {
     window.clarity('consent');
   }
